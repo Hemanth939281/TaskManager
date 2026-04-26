b4c0d481ea61677a253f80fa01288c5e55de33bb
Hemanth939281 <hkh777650@gmail.com>
2026-04-27 01:33:39 +0530

    changed backend deployed url in frontend

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
-  baseURL: "http://192.168.10.11:5000/api",
+  baseURL: "https://taskmanager-1mrd.onrender.com/api",
 });
 
 API.interceptors.request.use(async (req) => {
